1abf26d5edbe4c523cc6a3ae678ee7cc3eff60b1
Rishabh Singh <rishabh.singh@jbmgroup.com>
2023-03-06 16:31:05 +0530

    second commit

---
+++ cypress/e2e/DieMaintenanceTitle.cy.js
@@ -0,0 +1,8 @@
+it("verify title-positive", () => {
+    cy.visit("http://mm.thirdeye-ai.com/cmmsfrontend/login");
+    cy.title().should("eq", "Asset Maintenance");
+  });
+  it("verify title-negative test", () => {
+    cy.visit("http://mm.thirdeye-ai.com/cmmsfrontend/login");
+    cy.title().should("eq", "Assets Maintenance");
+  });
